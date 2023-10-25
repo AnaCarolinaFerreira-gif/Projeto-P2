06346b35c3430a526c16bd84d346539608fbc517
AnaCarolinaFerreira-gif <122412749+AnaCarolinaFerreira-gif@users.noreply.github.com>
2023-10-25 15:29:27 -0300

    Update MinhaRotas.js

--- my-app/src/MinhaRotas.js
+++ my-app/src/MinhaRotas.js
@@ -1,18 +1,19 @@
-import { BrowserRouter,Route, Routes } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Home from "./Home";
-import Atividade01 from "./Atividade 01";
-import Atividade02 from "./Atividade 02";
+import Home from"./Home";
 
-export default function MinhasRotas()
-{
+import Atividade01 from"./Atividade01";
+
+import Atividade02 from "./Atividade02";
+
+export default function MinhasRotas(){
     return(
         <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<Home/>} />
-            <Route path="/Atividade 01" element={<Atividade01/>} />
-            <Route path="/Atividade 02" element={<Atividade02/>} />
-        </Routes>
+            <Routes>
+                <Route path="/" element = {<Home />}/>
+                <Route path="/Atividade01" element = {<Atividade 01 />}/>
+                <Route path="/Atividade02" element = {<Atividade 02 />}/>
+            </Routes>
         </BrowserRouter>
     );
 }
